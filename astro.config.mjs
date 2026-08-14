2ec32e3050143626c6019a38af0c4ecdf837b4bf
wataru0531 <obito0531@gmail.com>
2026-08-14 13:28:52 +0900

    viteのconfigをkopi-

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,129 @@
 
 
 // @ts-check
+// import { defineConfig } from "astro/config"
+// import glsl from "vite-plugin-glsl"
+// import glslify from "rollup-plugin-glslify";
+
+
+// export default defineConfig({
+//   devToolbar: {
+//     enabled: false,
+//   },
+
+//   vite: {
+//     plugins: [
+//       glslify(),
+//     ],
+//   },
+
+//   server: {
+//     host: true,
+//   },
+// });
+
+
+
+// ⭐️ここから⭐️ここから
+
 import { defineConfig } from "astro/config"
 import glsl from "vite-plugin-glsl"
 import glslify from "rollup-plugin-glslify";
+import { resolve } from "path";
 
+// import { splitVendorChunkPlugin } from "vite";
+
+const root = "src";
 
 export default defineConfig({
+  root,
+  base: "",
+  publicDir: "../public",
+
   devToolbar: {
     enabled: false,
   },
 
-  vite: {
-    plugins: [
-      glslify(),
+  // vite: {
+  //   plugins: [
+  //     glslify({
+  //       compress(code) {
+  //         // Based on https://github.com/vwochnik/rollup-plugin-glsl
+  //         // Modified to remove multiline comments. See #16
+  //         let needNewline = false;
+  //         return code
+  //           .replace(/\\(?:\r\n|\n\r|\n|\r)|\/\*.*?\*\/|\/\/(?:\\(?:\r\n|\n\r|\n|\r)|[^\n\r])*/gs, "")
+  //           .split(/\n+/)
+  //           .reduce((result, line) => {
+  //             line = line.trim().replace(/\s{2,}|\t/, " "); // lgtm[js/incomplete-sanitization]
+  //             if (line.charAt(0) === "#" || /else/.test(line)) {
+  //               if (needNewline) {
+  //                 result.push("\n");
+  //               }
+  //               result.push(line, "\n");
+  //               needNewline = false;
+  //             } else {
+  //               result.push(line.replace(/\s*({|}|=|\*|,|\+|\/|>|<|&|\||\[|\]|\(|\)|-|!|;)\s*/g, "$1"));
+  //               needNewline = true;
+  //             }
+  //             return result;
+  //           }, [])
+  //           .join(process.env.NODE_ENV === "development" ? "\n" : "")
+  //           .replace(/\n+/g, "\n");
+  //       },
+  //     }),
+  //   ],
+  // },
+
+
+  plugins: [
+    // splitVendorChunkPlugin(),
+    glslify({
+      compress(code) {
+        // Based on https://github.com/vwochnik/rollup-plugin-glsl
+        // Modified to remove multiline comments. See #16
+        let needNewline = false;
+        return code
+          .replace(/\\(?:\r\n|\n\r|\n|\r)|\/\*.*?\*\/|\/\/(?:\\(?:\r\n|\n\r|\n|\r)|[^\n\r])*/gs, "")
+          .split(/\n+/)
+          .reduce((result, line) => {
+            line = line.trim().replace(/\s{2,}|\t/, " "); // lgtm[js/incomplete-sanitization]
+            if (line.charAt(0) === "#" || /else/.test(line)) {
+              if (needNewline) {
+                result.push("\n");
+              }
+              result.push(line, "\n");
+              needNewline = false;
+            } else {
+              result.push(line.replace(/\s*({|}|=|\*|,|\+|\/|>|<|&|\||\[|\]|\(|\)|-|!|;)\s*/g, "$1"));
+              needNewline = true;
+            }
+            return result;
+          }, [])
+          .join(process.env.NODE_ENV === "development" ? "\n" : "")
+          .replace(/\n+/g, "\n");
+      },
+    }),
+  ],
+
+  resolve: {
+    alias: [
+      // import { Inode } from "#/helper"; のように、
+      // 「/scripts」を「#」に置き換えることができる
+      {
+        find: "#",
+        replacement: "/scripts",
+      },
     ],
+  },
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        index: resolve(root, "index.html")
+      },
+    },
   },
 
   server: {
